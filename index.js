a8d7d2871cdddbf93935adc99896e211b0d079e2
Hemant Chouhan <hemant.chouhan123@gmail.com>
2022-01-29 20:01:12 +0530

    updated styling

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 const template = require('./template');
 const requestListener = async function (req, res) {
-    res.writeHead(200, { 'content-type': 'text/html' });
     const urlParams = url.parse(req.url, true);
+    console.log(urlParams);
+    if (urlParams.pathname.includes('/public/')) {
+        try {
+            const data = fs.readFileSync(__dirname + req.url);
+            res.writeHead(200);
+            res.end(data);
+            return;
+        } catch (error) {
+            res.writeHead(404);
+            res.end(JSON.stringify(error));
+            return;
+        }
+    }
+    res.writeHead(200, { 'content-type': 'text/html' });
     if (urlParams.query.method && urlParams.query.method === 'download') {
         const youtubedl = require('youtube-dl-exec');
         const details = await youtubedl(urlParams.query.url, {
--- template.js
+++ template.js
@@ -1,44 +1,85 @@
 const Handlebars = require('handlebars');
 const helpers = require('./helpers').functions;
-
+var source = (html) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/style.css">
+    <link rel="stylesheet" href="/public/bootstrap.min.css">
+    <title>Youtube Downloader</title>
+</head>
+<body>
+    <div class="container">
+        <div class="navbar navbar-dark bg-dark shadow-sm">
+            <div class="container d-flex justify-content-between">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <strong>Youtube Downloader</strong>
+            </a>
+            </div>
+        </div>
+    ${html}
+    </div>
+</body>
+</html>`;
+/*
+<p>Video: {{title}} </p>
+                    <p>Channel: {{channel}}</p>
+                    <p>Description: {{description}}</p>
+*/
 Handlebars.registerHelper("formatBytes", helpers.formatBytes);
-exports.createDownladpage = (data) =>{
-    var source = `
-                <button onclick="history.back()">Back</button> 
-                <p>Video: {{title}} </p>
-                 <p>Channel: {{channel}}</p>
-                 <p>Description: {{description}}</p>
-                    <table width='100%' border='1' cellpadding='10' cellspacing='10'>
-                        <thead>
-                            <td>Format</td>
-                            <td>Size</td>
-                            <td>Download</td>
-                        </thead>
-                        {{#formats}}
-                        <tr>
-                            <td>{{format}}</td>
-                            <td>{{formatBytes filesize}}</td>
-                            <td><a href='{{url}}' target='_blank' download>{{../title}}-{{format_note}}.{{ext}}</a></td>
-                        </tr>
-                        {{/formats}}
-                    </table>
-                    <script>
-                    </script>
-                `;
-    var template = Handlebars.compile(source);
+exports.createDownladpage = (data) => {
+    var html = source(`
+                    <div class="container">
+                    <div class="row">&nbsp;</div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <table width='100%' border='1' cellpadding='10' cellspacing='10'>
+                                <thead>
+                                    <td>Format</td>
+                                    <td>Size</td>
+                                    <td>Download</td>
+                                </thead>
+                                {{#formats}}
+                                <tr>
+                                    <td>{{format}}</td>
+                                    <td>{{formatBytes filesize}}</td>
+                                    <td><a href='{{url}}' target='_blank' download>{{../title}}-{{format_note}}.{{ext}}</a></td>
+                                </tr>
+                                {{/formats}}
+                            </table>
+                        </div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row">&nbsp;</div>
+                    </div>
+                `);
+    var template = Handlebars.compile(html);
     var result = template(data);
     return result;
 }
-exports.createForm = ()=>{
-    const source = `
-    <form style="margin:20%">
-      <label for="url">URL:</label><br>
-      <input type="text" id="url" name="url" required value="" placeholder="https://www.youtube.com/watch?v=-DYEW-UMUe0"><br>
-      <input type="hidden" name="method" value="download">
-      <input type="submit" value="Submit">
-    </form>
-    `;
-    var template = Handlebars.compile(source);
+exports.createForm = () => {
+    var html = source(`
+        <div class="container">
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+
+        <div class="row"></div>
+
+            <div class="row">
+                <div class="col-md-9">
+                    <form>
+                        <div class="form-group">
+                            <label for="url">Enter Url</label>
+                            <input type="text" name="url" class="form-control" id="url" required placeholder="https://www.youtube.com/watch?v=-DYEW-UMUe0">
+                        </div>
+                        <input type="hidden" name="method" value="download">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>`);
+    var template = Handlebars.compile(html);
     var result = template();
     return result;
 }
