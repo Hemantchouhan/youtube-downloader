8dd9d02275f8b0501986c3bb63799a281bfa3fb8
Hemant Chouhan <hemant.chouhan123@gmail.com>
2022-01-29 17:49:29 +0530

    updating routes

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const http = require('http');
 const url = require('url');
 const template = require('./template');
-
-
 const requestListener = async function (req, res) {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  const urlParams = url.parse(req.url,true);
-  if(urlParams.pathname === '/call'){
-    if(urlParams.query.method === 'get'){
-        const html = template.createForm();
-        return res.end(html);
-    }
-    if(urlParams.query.method === 'download'){
+    res.writeHead(200, { 'content-type': 'text/html' });
+    const urlParams = url.parse(req.url, true);
+    if (urlParams.pathname === '/call' && urlParams.query.method === 'download') {
         const youtubedl = require('youtube-dl-exec');
         const details = await youtubedl(urlParams.query.url, {
             dumpSingleJson: true,
@@ -24,9 +17,8 @@ const requestListener = async function (req, res) {
         const html = template.createDownladpage(details);
         return res.end(html);
     }
-    return res.end();
-  }
-  res.end('Api works');
+    const html = template.createForm();
+    return res.end(html);
 }
 
 const server = http.createServer(requestListener);
